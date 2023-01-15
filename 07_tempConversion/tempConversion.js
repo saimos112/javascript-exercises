6f800f7f9fa8e33e5f9e8301b0f508d58edb0ddd
saimos112 <tanavotss@gmail.com>
2023-01-15 16:36:55 +0200

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = roundTo1Decimal((temp - 32) * 0.5556);
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahren = roundTo1Decimal(temp * 1.8 + 32);
+  return fahren;
 };
+
+function roundTo1Decimal (num) { 
+    const number = parseFloat(num);
+    return Math.round(number * 10) / 10;
+  }
+  
 
 // Do not edit below this line
 module.exports = {
